868306f69bca0d8cd500be55c0aa697ad7e7578f
Seth Leyens <sel6606@g.rit.edu>
2019-02-19 18:24:18 -0500

    Obstacles now spawn and move towards the player

---
+++ EggDrop/Assets/Scripts/ObstacleManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleManager : MonoBehaviour
+{
+    public GameObject branchPrefab;
+    public float spawnRate;
+    public GameObject ball;
+
+    private float timer;
+
+	// Use this for initialization
+	void Start ()
+    {
+        timer = spawnRate;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            timer = spawnRate;
+            SpawnObstacle();
+        }
+	}
+
+    public void SpawnObstacle()
+    {
+        float spawnRotation = Random.Range(0, 359);
+        GameObject newObstacle = Instantiate(branchPrefab, Vector3.zero, Quaternion.AngleAxis(spawnRotation, Vector3.up), transform);
+    }
+
+    public void RotateWorld(float degreesToRotate)
+    {
+        gameObject.transform.Rotate(0, degreesToRotate, 0);
+    }
+}
---
+++ EggDrop/Assets/Scripts/ObstacleMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMove : MonoBehaviour {
+
+    public float moveSpeed;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        gameObject.transform.transform.localPosition += new Vector3(0, moveSpeed * Time.deltaTime, 0);
+
+
+        float distance = Vector3.Distance(transform.position, Camera.main.transform.position) - 0.5f;
+
+        Color temp = gameObject.GetComponent<MeshRenderer>().material.color;
+        temp.a = Mathf.Clamp01(1.0f - (1.0f / distance));
+
+        if(temp.a == 0)
+        {
+            Destroy(gameObject);
+        }
+        gameObject.GetComponent<Renderer>().material.color = temp;
+	}
+}
