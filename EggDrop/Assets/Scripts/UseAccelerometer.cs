b2bdbedefa3e2ee7ca04731ffbbcf06b7134609b
nac5961 <nac5961@g.rit.edu>
2019-02-20 13:54:58 -0500

    Tried to recalibrate the accelerometer based on the orientation of the phone. Not done yet though.

--- EggDrop/Assets/Scripts/UseAccelerometer.cs
+++ EggDrop/Assets/Scripts/UseAccelerometer.cs
@@ -10,6 +10,8 @@ public class UseAccelerometer : MonoBehaviour {
     public List<GameObject> obstacles;
     public float rotationSpeed;
 
+    private Quaternion calibration;
+
     // Use this for initialization
     void Start () {
         //Use the Gyroscope if a device has it, since rotation works better than the Accelerometer
@@ -17,12 +19,42 @@ public class UseAccelerometer : MonoBehaviour {
         {
             Input.gyro.enabled = true;
         }
+        else
+        {
+            CalibrateAccelerometer();
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
         RotateObstacles();
 	}
+
+    /// <summary>
+    /// Stores variables necessary to re-calibrate accelerometer input according
+    /// to the device's initial orientation.
+    /// </summary>
+    private void CalibrateAccelerometer()
+    {
+        //Get the device's initial movement data
+        Vector3 initialAcceleration = Input.acceleration;
+
+        //Get the rotation from this acceleration
+        Quaternion initialRotation = Quaternion.FromToRotation(-Vector3.forward, initialAcceleration);
+
+        //Store the calibrated data
+        calibration = Quaternion.Inverse(initialRotation);
+    }
+
+    /// <summary>
+    /// Adjusts the acceleration data based on the calibration/
+    /// </summary>
+    /// <param name="acceleration">acceleration vector from the Accelerometer</param>
+    /// <returns>the adjusted acceleration</returns>
+    private Vector3 AdjustedMovement(Vector3 acceleration)
+    {
+        return calibration * acceleration;
+    }
 
     /// <summary>
     /// Uses the accelerometer to rotate all obstacles
@@ -41,7 +73,8 @@ public class UseAccelerometer : MonoBehaviour {
             //Default to Accelerometer
             else
             {
-                obstacles[i].transform.Rotate(0.0f, Input.acceleration.y, 0.0f);
+                Vector3 calibratedAcceleration = AdjustedMovement(Input.acceleration);
+                obstacles[i].transform.Rotate(0.0f, calibratedAcceleration.x, 0.0f);
             }
         }
 
