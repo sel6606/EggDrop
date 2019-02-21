075f0b62b45a6b1ab7479ddfab5eb5c70f2a9033
Max Kaiser <MaxRKaiser18@gmail.com>
2019-02-21 02:30:30 -0500

    Added main menu scene, ability to load into main scene, ability to toggle how to play, text mesh pro import

---
+++ EggDrop/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+    public GameObject howToPanel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void loadGame()
+    {
+        SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
+    }
+
+    public void toggleHowTo()
+    {
+        if (!howToPanel.activeInHierarchy)
+        {
+            howToPanel.SetActive(true);
+        }
+        else
+        {
+            howToPanel.SetActive(false);
+        }
+    }
+}
