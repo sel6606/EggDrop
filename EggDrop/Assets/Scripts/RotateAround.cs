bc69fa5306f016c820fa350eda19f29877b67c72
nac5961 <nac5961@g.rit.edu>
2019-02-27 13:49:27 -0500

    Created new obstacle prefabs and scripts to have obstacles with different actions.

---
+++ EggDrop/Assets/Scripts/RotateAround.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Script to constantly rotate the obstacles.
+/// </summary>
+public class RotateAround : MonoBehaviour {
+
+    public float rotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RotateObstacle();	
+	}
+
+    /// <summary>
+    /// Rotates the obstacle.
+    /// </summary>
+    private void RotateObstacle()
+    {
+        float angle = rotationSpeed * Time.deltaTime;
+
+        transform.Rotate(Vector3.right, angle);
+    }
+}
---
+++ EggDrop/Assets/Scripts/RotateHalfway.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Script to constantly rotate the obstacle back and forth 180 degrees.
+/// </summary>
+public class RotateHalfway : MonoBehaviour {
+
+    public float rotationSpeed;
+
+    private Quaternion startRot;
+    private Quaternion targetRot;
+
+    public float time;
+    private bool movingToTarget;
+
+	// Use this for initialization
+	void Start () {
+        startRot = transform.rotation;
+        targetRot = startRot * Quaternion.Euler(180.0f, 0.0f, 0.0f);
+        movingToTarget = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RotateObstacle();
+	}
+
+    /// <summary>
+    /// Rotates obstacle back and forth by 180 degrees from its start rotation.
+    /// </summary>
+    private void RotateObstacle()
+    {
+        time += Time.deltaTime;
+
+        Mathf.Clamp(time, 0.0f, 1.0f);
+
+        if (movingToTarget)
+        {
+            Quaternion.Slerp(startRot, targetRot, Time.deltaTime);
+        }
+        else
+        {
+            Quaternion.Slerp(targetRot, startRot, Time.deltaTime);
+        }
+
+        if (time >= 1.0f)
+        {
+            time = 0.0f;
+            movingToTarget = !movingToTarget;
+            transform.rotation = targetRot;
+        }
+    }
+}
