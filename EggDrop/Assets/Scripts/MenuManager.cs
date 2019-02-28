529c47ed98f7898352f289d0005f86cb7881b14a
Max Kaiser <MaxRKaiser18@gmail.com>
2019-02-27 23:32:15 -0500

    Added score to game screen, added pause menu, added gameover menu

--- EggDrop/Assets/Scripts/MenuManager.cs
+++ EggDrop/Assets/Scripts/MenuManager.cs
@@ -2,24 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MenuManager : MonoBehaviour {
 
     public GameObject howToPanel;
+    public GameObject pausePanel;
+    public GameObject gameOverPanel;
+    public GameObject scoreTextHolder;
+    public TextMeshProUGUI scoreText;
+    public GameObject scoreTextHolder2;
+    public TextMeshProUGUI scoreText2;
+    public float score;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        score = 0;
+        scoreText = scoreTextHolder.GetComponent<TextMeshProUGUI>();
+        scoreText2 = scoreTextHolder2.GetComponent<TextMeshProUGUI>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            togglePause();
+        }
+
+        scoreText.SetText("You fell: {0} meters!", score);
+        scoreText2.SetText("Distance: {0}m", score);
+    }
 
     public void loadGame()
     {
         SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
+    }
+
+    public void loadMenu()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
     }
 
     public void toggleHowTo()
@@ -31,6 +52,30 @@ public class MenuManager : MonoBehaviour {
         else
         {
             howToPanel.SetActive(false);
+        }
+    }
+
+    public void togglePause()
+    {
+        if (!pausePanel.activeInHierarchy)
+        {
+            pausePanel.SetActive(true);
+        }
+        else
+        {
+            pausePanel.SetActive(false);
+        }
+    }
+
+    public void toggleGameOver()
+    {
+        if (!gameOverPanel.activeInHierarchy)
+        {
+            gameOverPanel.SetActive(true);
+        }
+        else
+        {
+            gameOverPanel.SetActive(false);
         }
     }
 }
